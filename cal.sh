5f2b6ea632bc82d8a722c59e37d4f4d734412241
Nilimagit <146575671+Nilimagit@users.noreply.github.com>
2024-03-10 10:45:57 +0530

    add cal in main

---
+++ cal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#addtion function
+x=a+b
+#substraction function
+x= a-b
+#division function
+#x= a%b
+HEAD
+echo "this is new commit"
+echo "lets do it"
+echo "checks"
+echo "do this"
+
+#echo "new1"
+ 
